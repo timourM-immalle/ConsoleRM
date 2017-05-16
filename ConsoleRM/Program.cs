b9a7069bbc0979876190c082365543716788babb
timour.meeusen <timour.meeusen@B209-11>
2017-05-16 12:57:41 +0200

    //opgave

--- ConsoleRM/Program.cs
+++ ConsoleRM/Program.cs
@@ -10,6 +10,16 @@ namespace ConsoleRM
     {
         static void Main(string[] args)
         {
+            /*
+             * Schrijf een Console-programma dat een tekstfile input.txt inleest, b.v.
+3 + 5
+8*6
+89*10
+en vervolgens een file output.txt wegschrijft met daarin de resultaten:
+8
+42
+890
+Extra: zorg ervoor dat je het programma op de console kan uitvoeren en met een parameter --input inputfile.txt en --output outputfile.txt beide files kan meegeven. (TIP: hiervoor zul je de array string[] args moeten gebruiken van de Main-method. Uittesten zal je moeten doen via b.v. powershell)*/
         }
     }
 }
